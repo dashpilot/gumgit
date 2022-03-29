28666fcfeb7f728d4e3e4229d1d418b7b433466c
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 15:06:54 +0200

    Update download.js

--- api/download.js
+++ api/download.js
@@ -7,32 +7,7 @@ const dir = "./dist/";
 
 export default async function handler(request, response) {
   
-const api = new Gumnode({
-  access_token: process.env.GR_TOKEN,
-});
-  
-api
-  .getUserSales({ request.query.sale_id })
-  .then((res) => {
-  
-    console.log('sale verified');
-    console.log(res);
-
-    var filenames = fs.readdirSync(dir);
-    filenames.forEach(file => {
-      console.log(file);
-      zip.addLocalFile(dir+file);
-    });
-
-    var buffer = zip.toBuffer();
-
-    response.setHeader("Content-Type", "application/octet-stream");
-    response.setHeader("Content-Disposition", "inline;filename=myfile.zip");
-    response.end(buffer);
-  
-})
-  .catch((res) => console.log("Error:", res.message));
-  
+response.end('hi');
  
   
 }
