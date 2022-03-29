f774a39f1b85fc92152fa51927302e896b2e201f
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 14:50:41 +0200

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,29 +1,39 @@
 const fs = require('fs');
 var AdmZip = require("adm-zip");
 var zip = new AdmZip();
+const Gumnode = require("gumnode");
+
+const api = new Gumnode({
+  access_token: process.env.GR_TOKEN,
+});
 
 const dir = "./dist/";
 
 export default async function handler(request, response) {
   
-  var content = "inner content of the file";
-  zip.addFile("at-runtime.txt", Buffer.from(content, "utf8"), "entry comment goes here");
   
+api
+  .getUserSales({ request.query.sale_id })
+  .then((response) => {
   
-  var filenames = fs.readdirSync(dir);
-  filenames.forEach(file => {
-    console.log(file);
-    zip.addLocalFile(dir+file);
-  });
+    console.log('license verified');
+    console.log(response);
+
+    var filenames = fs.readdirSync(dir);
+    filenames.forEach(file => {
+      console.log(file);
+      zip.addLocalFile(dir+file);
+    });
+
+    var buffer = zip.toBuffer();
+
+    response.setHeader("Content-Type", "application/octet-stream");
+    response.setHeader("Content-Disposition", "inline;filename=myfile.zip");
+    response.end(buffer);
+  
+})
+  .catch((response) => console.log("Error:", response.message));
+  
  
-  
-  // zip.addLocalFile("./dist/index.html");
-
-  var buffer = zip.toBuffer();
-  
-  response.setHeader("Content-Type", "application/octet-stream");
-  response.setHeader("Content-Disposition", "inline;filename=myfile.zip");
-
-  response.end(buffer);
   
 }
